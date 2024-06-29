eea263f1cccb25959dac9c8d81dbb51813783c0f
_Ben <54240316+botdevXD@users.noreply.github.com>
2024-06-29 14:51:06 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,14 +5,17 @@ local games = {
 }
 
 local Dependencies = {
-
+    CG_AIM_VIEW_LIB = "DH_AIM_VIEWER",
+    CG_ESP_LIB = "CG_ESP_2024",
+    CG_FLY_LIB = "CG_FLY_SCRIPT",
+    CG_LOW_GFX_LIB = "CG_LOW_GFX_MODULE",
 }
 
 local Data = {
     GithubRepOwner = "botdevXD",
     GithubRepName = "CGHub",
     GamesFolder = "https://raw.githubusercontent.com/%s/%s/main/Games/",
-    DepsFolder = "https://raw.githubusercontent.com/%s/%s/main/Games/",
+    DepsFolder = "https://raw.githubusercontent.com/%s/%s/main/Dependencies/",
 }
 
 local currentPlaceId = game.PlaceId
@@ -28,6 +31,19 @@ local function getScriptSource(gameId)
     return fetchedSource and returnSource
 end
 
-if games[currentPlaceId] == nil then
+local function getDependencySource(depName)
+    local fetchedSource, returnSource pcall(function()
+        local depsFolderUrl = Data.DepsFolder:format(Data.GithubRepOwner, Data.GithubRepName)
+        local depSourceUrl = depsFolderUrl .. depName .. ".lua"
+
+        return game:HttpGet(depSourceUrl, true)
+    end)
+
+    return fetchedSource and returnSource
+end
+
+local currentGameSource = getScriptSource(currentPlaceId)
+if not currentGameSource then
+    warn("Failed to fetch game source")
     return
 end
