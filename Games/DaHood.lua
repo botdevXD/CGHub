2f551d8afef0d8e51962e56462250f0ec0a39f12
_Ben <54240316+botdevXD@users.noreply.github.com>
2025-01-10 05:00:31 +0000

    Update DaHood.lua

--- Games/DaHood.lua
+++ Games/DaHood.lua
@@ -1813,7 +1813,7 @@ task.spawn(function()
     MiscSectionRight:Slider({
         Name = "Drop Amount",
         Minimum = 100,
-        Maximum = 15075,
+        Maximum = 15076,
         Default = shared.CG_DA_HOOD_CONFIG_TABLE.AUTO_DROP_CASH_AMOUNT,
         flag = "moneydropamountflag",
         Callback = function(Value)
